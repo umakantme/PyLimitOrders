dc7384c10cfd94d176baa15d3b9cbf07b897f95b
Umakant Mekhale <umakantm1@kpit.com>
2024-04-02 19:35:25 +0530

    modified code added for the PyLimitOrders

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -6,7 +6,6 @@ class LimitOrderAgent(PriceListener):
 
     def __init__(self, execution_client: ExecutionClient) -> None:
         """
-
         :param execution_client: can be used to buy or sell - see ExecutionClient protocol definition
         """
         super().__init__()
@@ -22,13 +21,28 @@ class LimitOrderAgent(PriceListener):
         }
         self.orders.append(order)
 
-    def on_price_tick(self, product_id: str, price: float):
-        # see PriceListener protocol and readme file
+    def execute_order(self):
+
+        self.product_id, self.price = PriceListener.on_price_tick(self)
         for order in self.orders:
-            if (order['buy_flag'] and price < order['limit']):
+            if (order['buy_flag'] and self.price < order['limit']):
                 ExecutionClient.buy(self,order['product_id'],order['amount'])
-            elif not order['buy_flag'] and price > order['limit']:
+                order['execution_flag'] = True
+            elif not order['buy_flag'] and self.price > order['limit']:
                 ExecutionClient.sell(self,order['product_id'],order['amount'])
+                order['execution_flag'] = True
+
+        # remove the added orders after they have been executed for either buy or sell
+        self.orders = [item for item in self.orders if not item.get('execution_flag')]
+
+    #retrying unexecuted orders needs to be added so that whenever the market price condition is satisfied it should be executed immediately afterwards
+    def unexecuted_orders(self):
+        if self.orders:
+            self.execute_order()
+        else:
+            print("All orders executed successfully...")
+
+
 
 if __name__ == "__main__":
 
@@ -40,15 +54,11 @@ if __name__ == "__main__":
         # Add buy order for 1000 shares of IBM at $100 limit
         agent.add_order(True, 'IBM', 1000, 100)
 
-        # Simulate price ticks
-        agent.on_price_tick('IBM', 99)  # Order should not be executed
-        agent.on_price_tick('IBM', 101)  # Order should be executed
-
         # Add sell order for 1000 shares of IBM at $150 limit
         agent.add_order(False, 'IBM', 1000, 150)
 
-        # Simulate price ticks
-        agent.on_price_tick('IBM', 160)  # Order should be executed
+        agent.execute_order()
+        agent.unexecuted_orders()
 
     except ExecutionException as e:
         print("An error occurred:", e)
--- trading_framework/price_listener.py
+++ trading_framework/price_listener.py
@@ -3,13 +3,19 @@ from typing import Protocol
 
 class PriceListener(Protocol):
 
-    def on_price_tick(self, product_id: str, price: float):
+    def __init__(self):
+        pass
+
+    def on_price_tick(self):
         """
         invoked on market data change
         :param product_id: id of the product that has a price change
         :param price: the current market price of hte product
         :return: None
         """
+        self.product_id = input("Enter the product id : ")
+        self.price = int(input("Enter the price : "))
+        print("Current market price of the Product {0} : Price {1}".format(self.product_id, self.price))
 
-        print(f"Received price tick for product {product_id}: ${price}")
+        return self.product_id, self.price
 
